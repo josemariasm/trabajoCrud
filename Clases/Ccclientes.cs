c89c77862122bd533abeefb2ca79649d0030ff01
Josemariasm <josemariasanchezmartinez@gmail.com>
2025-03-27 20:23:10 +0100

    subir

---
+++ Clases/Ccclientes.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace VetManager_0._1.Clases
+{
+    internal class Ccclientes
+    {
+        public void MostrarClientes(DataGridView dgvTotalClientes)
+        {
+            try
+            {
+                CConexion objetoConexion = new CConexion();
+
+
+                String query = "select * from dueño";
+                dgvTotalClientes.DataSource = null;
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, objetoConexion.establecerConexion());
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvTotalClientes.DataSource = dt;
+                objetoConexion.cerrarConexion();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo realizar la conexion, error:" + ex.ToString());
+            }
+
+        }
+
+        public void GuardarClientes(TextBox Id, TextBox Nombre, TextBox Apellidos, TextBox Domicilio, TextBox Telefono)
+        {
+            try
+            {
+                CConexion objetoConexion = new CConexion();
+
+
+                String query = "insert into dueño (idDueño,nombre,apellidos,domicilio,telefono)" + "values('" + Id.Text + "','" + Nombre.Text + "','" + Apellidos.Text + "','" + Domicilio.Text + "', '" + Telefono.Text + "');";
+
+                MySqlCommand myComannd = new MySqlCommand(query, objetoConexion.establecerConexion());
+                MySqlDataReader reader = myComannd.ExecuteReader();
+                MessageBox.Show("Se guardaron los registros correcgamente");
+                while (reader.Read())
+                {
+
+                }
+                objetoConexion.cerrarConexion();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo realizar la conexion, error:" + ex.ToString());
+            }
+        }
+
+        public void SeleccionarClientes(DataGridView tablaClientes, TextBox Id, TextBox Nombre, TextBox Apellidos, TextBox Domicilio, TextBox Telefono)
+        {
+            try
+            {
+                Id.Text= tablaClientes.CurrentRow.Cells[0].Value.ToString();
+                Nombre.Text = tablaClientes.CurrentRow.Cells[1].Value.ToString();
+                Apellidos.Text = tablaClientes.CurrentRow.Cells[2].Value.ToString();
+                Domicilio.Text = tablaClientes.CurrentRow.Cells[3].Value.ToString();
+                Telefono.Text = tablaClientes.CurrentRow.Cells[4].Value.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo seleccionar, error:" + ex.ToString());
+            }
+        }
+
+        public void ModificarClientes(TextBox Id, TextBox Nombre, TextBox Apellidos, TextBox Domicilio, TextBox Telefono)
+        {
+            try
+            {
+                CConexion objetoConexion = new CConexion();
+
+
+                String query = "update dueño set nombre='" + Nombre.Text + "',apellidos='" + Apellidos.Text + "',domicilio='" + Domicilio.Text + "',telefono='" + Telefono.Text + "' where idDueño ='" + Id.Text + "';";
+
+                MySqlCommand myComannd = new MySqlCommand(query, objetoConexion.establecerConexion());
+                MySqlDataReader reader = myComannd.ExecuteReader();
+                MessageBox.Show("Se actualizaron los registros correcgamente");
+                while (reader.Read())
+                {
+
+                }
+                objetoConexion.cerrarConexion();
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo actualizar, error:" + ex.ToString());
+            }
+        }
+
+        public void EliminarClientes(TextBox Id)
+        {
+            try
+            {
+                CConexion objetoConexion = new CConexion();
+
+
+                String query = "delete from dueño where idDueño= '" + Id.Text + "';";
+
+                MySqlCommand myComannd = new MySqlCommand(query, objetoConexion.establecerConexion());
+                MySqlDataReader reader = myComannd.ExecuteReader();
+                MessageBox.Show("Se eliminarón los registros correcgamente");
+                while (reader.Read())
+                {
+
+                }
+                objetoConexion.cerrarConexion();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo eliminarón los datos, error:" + ex.ToString());
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetManager_0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Clases.Ccclientes objetoUsuarios = new Clases.Ccclientes();
+            objetoUsuarios.MostrarClientes(dgvTotalClientes);
+            // Establece el título del formulario
+            this.Text = "VetManager - Gestión Veterinaria";
+
+        }
+        private void LimpiarControles()
+        {
+            txtId.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtApellido.Text = string.Empty;
+            txtDomicilio.Text = string.Empty;
+            txtTelefono.Text = string.Empty;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // Clases.CConexion objetoConexion = new Clases.CConexion();
+            //objetoConexion.establecerConexion();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Clases.Ccclientes objetoUsuarios = new Clases.Ccclientes();
+            objetoUsuarios.GuardarClientes(txtId,txtNombre,txtApellido,txtDomicilio,txtTelefono);
+            objetoUsuarios.MostrarClientes(dgvTotalClientes);
+            LimpiarControles();
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvTotalClientes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Clases.Ccclientes objetoUsuarios = new Clases.Ccclientes();
+            objetoUsuarios.SeleccionarClientes(dgvTotalClientes,txtId,txtNombre,txtApellido,txtDomicilio,txtTelefono);
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            Clases.Ccclientes objetoUsuarios = new Clases.Ccclientes();
+            objetoUsuarios.ModificarClientes(txtId, txtNombre, txtApellido, txtDomicilio, txtTelefono);
+            objetoUsuarios.MostrarClientes(dgvTotalClientes);
+            LimpiarControles();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Clases.Ccclientes objetoUsuarios = new Clases.Ccclientes();
+            objetoUsuarios.EliminarClientes(txtId);
+            objetoUsuarios.MostrarClientes(dgvTotalClientes);
+            LimpiarControles();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvTotalClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
